4be20f7aef7e2f7d399e12ebde2debb29aad54fb
deadprogram <ron@hybridgroup.com>
2024-12-03 14:31:31 +0100

    targets: handle abort by triggering TKey device fault using illegal instruction to halt CPU
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- src/runtime/runtime_tkey_baremetal.go
+++ src/runtime/runtime_tkey_baremetal.go
@@ -2,6 +2,8 @@
 
 package runtime
 
+import "device/riscv"
+
 // ticksToNanoseconds converts ticks (at 18MHz) to nanoseconds.
 func ticksToNanoseconds(ticks timeUnit) int64 {
 	return int64(ticks) * 10000
@@ -17,8 +19,6 @@ func exit(code int) {
 }
 
 func abort() {
-	// lock up forever
-	for {
-		// TODO: something here?
-	}
+	// Force illegal instruction to halt CPU
+	riscv.Asm("unimp")
 }
